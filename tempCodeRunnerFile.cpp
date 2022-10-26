5f3c511dc916b184375fe6a058a549a594d40bfd
Jagpreet <21bcs1337@cuchd.in>
2022-10-26 17:46:50 +0530

    deleted

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-int index;
-    // cout << "Enter the index at which you want to add.";
-    // cin >> index;
-    // int element;
-    // cout << "Enter the element: " << endl;
-    // cin >> element;
